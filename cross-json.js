32a389d394d453da00aa09422c9f6d0d89dd49bf
kdex <kdex@kdex.de>
2015-07-01 06:43:16 +0200

    Added module.exports support
    This change probably breaks grunt-cross-json, but allows a cleaner way of tackling gulp-cross-json.

--- cross-json.js
+++ cross-json.js
@@ -44,102 +44,101 @@ function compare(a, b) {
 	}
 }
 function crossCompare(args) {
-	return Promise.all(args.map(function(file) {
-		return new Promise(function(resolve, reject) {
-			return fs.exists(file, function(exists) {
-				if (exists) {
-					resolve(file);
-				}
-				else {
-					throw Error(`${args[0]}: ${file}: No such file or directory`);
-				}
-			});
-		});
-	})).then(function(files) {
-		Promise.all(files.map(function(file) {
+	return new Promise(function(finalResolve) {
+		return Promise.all(args.map(function(file) {
 			return new Promise(function(resolve, reject) {
-				fs.realpath(file, function(e, realPath) {
-					fs.stat(realPath, function(statError, stats) {
-						if (statError) {
-							throw Error(statError);
-						}
-						else {
-							if (stats.isFile()) {
-								resolve({
-									type: FILE,
-									path: file
-								});
-							}
-							else if (stats.isDirectory()) {
-								resolve({
-									type: DIRECTORY,
-									path: file
-								});
+				return fs.exists(file, function(exists) {
+					if (exists) {
+						resolve(file);
+					}
+					else {
+						throw Error(`${args[0]}: ${file}: No such file or directory`);
+					}
+				});
+			});
+		})).then(function(files) {
+			Promise.all(files.map(function(file) {
+				return new Promise(function(resolve, reject) {
+					fs.realpath(file, function(e, realPath) {
+						fs.stat(realPath, function(statError, stats) {
+							if (statError) {
+								throw Error(statError);
 							}
 							else {
-								throw Error(`${file} is not a regular file`);
-							}
-						}
-					});
-				});
-			});
-		})).then(function(list) {
-			Promise.all(list.map(function(element) {
-				return new Promise(function(resolve, reject) {
-					switch (element.type) {
-						case FILE:
-							fs.readFile(element.path, "utf-8", function(readError, fileContent) {
-								if (readError) {
-									throw Error(readError);
+								if (stats.isFile()) {
+									resolve({
+										type: FILE,
+										path: file
+									});
+								}
+								else if (stats.isDirectory()) {
+									resolve({
+										type: DIRECTORY,
+										path: file
+									});
 								}
 								else {
-									try {
-										resolve({
-											json: JSON.parse(fileContent),
-											element: element
-										});
-									}
-									catch (jsonError) {
-										throw Error(`Invalid JSON in file ${element.path}: ${jsonError}`);
-									}
+									throw Error(`${file} is not a regular file`);
 								}
-							});
-							break;
-						case DIRECTORY:
-							/* TODO: Add support for specifying directories */
-							reject("ERR_DIRECTORY");
-							break;
-					}
-				});
-			})).then(function(jsonContent) {
-				jsonContent.forEach(function(content, i) {
-					let sourceFile = content.element.path.substr(content.element.path.lastIndexOf("/") + 1);
-					for (let j = 0; j < jsonContent.length;  ++j) {
-						if (i === j) {
-							continue;
-						}
-						let comparisonFile = jsonContent[j].element.path.substr(jsonContent[j].element.path.lastIndexOf("/") + 1);
-						let sourceJSON = content.json;
-						let destinationJSON = jsonContent[j].json;
-						compare({
-							file: sourceFile,
-							json: content.json
-						}, {
-							file: comparisonFile,
-							json: destinationJSON
+							}
 						});
-					}
+					});
+				});
+			})).then(function(list) {
+				Promise.all(list.map(function(element) {
+					return new Promise(function(resolve, reject) {
+						switch (element.type) {
+							case FILE:
+								fs.readFile(element.path, "utf-8", function(readError, fileContent) {
+									if (readError) {
+										throw Error(readError);
+									}
+									else {
+										try {
+											resolve({
+												json: JSON.parse(fileContent),
+												element: element
+											});
+										}
+										catch (jsonError) {
+											throw Error(`Invalid JSON in file ${element.path}: ${jsonError}`);
+										}
+									}
+								});
+								break;
+							case DIRECTORY:
+								/* TODO: Add support for specifying directories */
+								reject("ERR_DIRECTORY");
+								break;
+						}
+					});
+				})).then(function(jsonContent) {
+					let counter = 0;
+					jsonContent.forEach(function(content, i, contents) {
+						let sourceFile = content.element.path.substr(content.element.path.lastIndexOf("/") + 1);
+						for (let j = 0; j < jsonContent.length;  ++j) {
+							if (i === j) {
+								continue;
+							}
+							let comparisonFile = jsonContent[j].element.path.substr(jsonContent[j].element.path.lastIndexOf("/") + 1);
+							let sourceJSON = content.json;
+							let destinationJSON = jsonContent[j].json;
+							compare({
+								file: sourceFile,
+								json: content.json
+							}, {
+								file: comparisonFile,
+								json: destinationJSON
+							});
+						}
+						++counter;
+						if (counter === contents.length) {
+							finalResolve();
+						}
+					});
 				});
 			});
 		});
 	});
 }
-let args = process.argv.filter(function(x) {
-	return x.endsWith(".json");
-});
-if (args.length < 2) {
-	displayHelp();
-}
-else {
-	crossCompare(args);
-}
+module.exports = crossCompare;
